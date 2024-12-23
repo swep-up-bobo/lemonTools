4c8aafa30c12531517c3643340606256e6ce3895
lemon <1505701010@qq.com>
2024-12-23 17:25:22 +0800

    first commit

---
+++ src/main/java/com/lemon/Main.java
@@ -0,0 +1,16 @@
+package com.lemon;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("lemon工具箱");
+        stage.show();
+    }
+}
